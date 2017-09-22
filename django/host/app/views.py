b4ed9b3018036206c703a547cedca90f53e87d06
NicholasPiano <nicholas.d.piano@gmail.com>
2017-09-22 21:26:32 +0100

    Basic redirect that can take a more specific uuid when the registry is set up

--- django/host/app/views.py
+++ django/host/app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def host_redirect(request):
+
+	# potentially hit database here for registry id
+	address = 'localhost:8081'
+	uuid = 'hello'
+
+	return redirect('http://{address}/{uuid}'.format(address=address, uuid=uuid))
